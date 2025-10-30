69b6ce4c3939fdc1af34f24a55a2e4346004a153
TaYLoR714 <treynolds011@student.butte.edu>
2025-10-30 17:49:53 +0000

    TaYLoR714

---
+++ queues.py
@@ -0,0 +1,15 @@
+#python
+import array as array
+
+
+#------
+#queues push things onto them
+def next_person(q,x):
+    q.apend(x)
+    return q
+
+#queues pop things off
+def give_bread(q):
+    return q.pop
+
+print ("hello world")

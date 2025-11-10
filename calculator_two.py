e4d3c3558529cceed6c38ef990c41661d2d48657
TaYLoR714 <treynolds011@student.butte.edu>
2025-11-10 20:29:38 +0000

    Calculatortwo

---
+++ calculator_two.py
@@ -0,0 +1,40 @@
+#python
+import array as array
+
+#Simple math calculator for my programming class
+
+a = 5
+b = 5
+sum = a + b
+print(sum)
+
+a = 1
+b = -1
+sum = a + b
+print(sum)
+
+
+a = 0
+b = -1
+sum = a - b
+print(sum)
+
+a = 0
+b = 5
+sum = a / b
+print(sum)
+
+a = 4
+b = 2
+sum = a / b
+print(sum)
+
+a = 2
+b = 1
+sum = a * b
+print(sum)
+
+a = 3
+b = 2
+sum = a * b
+print(sum)

c304d803e2d361f0d58bcb8ffbd3ae328f5d77c2
TaYLoR714 <treynolds011@student.butte.edu>
2025-11-17 03:12:41 +0000

    calcthree

--- calculator_three.py
+++ calculator_three.py
@@ -1,2 +1,24 @@
 #python
 import array as array
+
+def addition(num1: int, num2: int) -> None:
+    print(num1 + num2)
+
+def subtraction(num1: int, num2: int) -> None:
+    print(num1 - num2)
+
+def multiplication(num1: int, num2: int) -> None:
+    print(num1 * num2)
+
+def division(num1: int, num2: int) -> None:
+    if num2 == 0:
+        raise ValueError
+
+    print(num1 / num2)
+
+
+    #------------------------------------
+
+    operator = input("Enter and operator(+-*/): ")
+    num1 = input("enter the first number: ")
+    num2 = input("enter the second number: ")

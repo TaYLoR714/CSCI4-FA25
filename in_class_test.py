0e6c7b52fbf27ffd323b4344f2f52e37ff273c87
TaYLoR714 <treynolds011@student.butte.edu>
2025-11-25 18:03:25 +0000

    inclass

--- in_class_test.py
+++ in_class_test.py
@@ -13,3 +13,14 @@ def f(x,y,z):
 f(1,1,0) #guess sum 3
 f(1,2,1) #guess difference 1
 f(1,2,5) #guess string in the else statement
+
+
+x = 5  #x represents left right
+y = 6  #y represnets down up
+# index 0 represnes x coord, index 1 represents y coord
+point = [x,y]
+
+point_dict = {'x': 5, 'y': 6}
+
+print("point at 0:", point[0])
+print("point_dict at 'x':", point_dict['x'])
